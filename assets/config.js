2ef18be2a40b601eaf3fd07678d9384f3cbb4d9a
j4ck0b <portykuby@gmail.com>
2026-06-17 01:21:41 +0200

    refactor: centralize Web3Forms access key configuration

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+// Konfiguracja First Partners
+window.FP_CONFIG = {
+    // Wpisz tutaj swój darmowy klucz Web3Forms uzyskany z https://web3forms.com dla adresu ceo@firstpartners.pl
+    web3forms_key: "YOUR_ACCESS_KEY_HERE"
+};
